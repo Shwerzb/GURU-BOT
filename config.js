e632250811b643b55e9b8646b7a69e96e941a295
Shwerzb <153048058+Shwerzb@users.noreply.github.com>
2023-12-05 22:03:10 -0500

    Update config.js

--- config.js
+++ config.js
@@ -7,7 +7,7 @@ import axios from 'axios'
 
 
 global.owner = [
-  ['917605902011', 'GURU', true],
+  ['18454183643', 'Shmily Werzberger bot', true],
   ['6281255369012', 'Ash', true], 
   [''] 
 ] //Number of owners
